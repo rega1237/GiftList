a2a64c81095de37000306379f79ec5f1a59999ef
rega1237 <rega1237@gmail.com>
2023-11-14 08:24:10 -0400

    complete challenge merklee tree

--- client/index.js
+++ client/index.js
@@ -6,10 +6,22 @@ const serverUrl = 'http://localhost:1225';
 
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
+  const merkleTree = new MerkleTree(niceList);
+
+  const root = merkleTree.getRoot();
+
+  const name = "Traci McDermott";
+
+  const index = niceList.findIndex(n => n === name);
+
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
-  });
+    name: name,
+    proof: proof,
+    root: root
+  })
 
   console.log({ gift });
 }
--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ app.post('/gift', (req, res) => {
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+
+  const isInTheList = verifyProof(body.proof, body.name, body.root)
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
